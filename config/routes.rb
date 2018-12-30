92b656ba6e097dc3255feb9a651922c448c5a590
Brandon Caiza <brandoncaiza97@hotmail.com>
2018-12-30 01:46:47 -0800

    Adds

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :boxes
-  resources :password_resets
-
-  resources :orders
-  resources :line_items do
-    member do
-      get 'remove'
-    end
-  end
-
-  resources :carts 
-  resources :products
-
-  get '/mealbox' => 'products#mealbox'
-
-  get 'sessions/new'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
-  resources :users
-
 
   root to: 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
