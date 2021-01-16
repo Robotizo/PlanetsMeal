2655cbfdf2bc519b2667bf1ac961e351df6eb7de
Brandon Caiza <geniusrobotizo@gmail.com>
2021-01-16 01:59:11 -0800

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'brandon@planetsmeal.com',
-    password:             '@Ramosmanu7@!',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true  
   }
